16eb41244524568b8cc6df7326409937d6fb96c5
Lika Kharatishvili <Lika.Kharatishvili@ol.ge>
2020-06-16 15:56:12 +0400

    'githubApi'

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -1,7 +1,7 @@
 
 import { CoreModule } from "./core/core.module";
 import { SharedModule } from "./shared/shared.module";
-import { BrowserModule } from "@angular/platform-browser";
+import { BrowserModule, BrowserTransferStateModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AngularSplitModule } from "angular-split";
 import { AppRoutingModule } from "./app-routing.module";
@@ -9,30 +9,20 @@ import { AppComponent } from "./app.component";
 import { NotFoundComponent } from "./components/not-found/not-found.component";
 import { ContributorsComponent } from "./components/users/user/repositories/contributors/contributors.component";
 import { NgxPaginationModule } from "ngx-pagination";
-import { CommonModule } from '@angular/common';
-import { TransferHttpCacheModule } from '@nguniversal/common';
-import { HttpClientModule } from '@angular/common/http';
-import { NgtUniversalModule } from '@ng-toolkit/universal'; // <-- import the module
+import { AppModule } from './app.module'; // <-- import the module
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NotFoundComponent,
-    ContributorsComponent,
-  ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    
     AppRoutingModule,
     AngularSplitModule.forRoot(),
     NgxPaginationModule,
     SharedModule,
     CoreModule,
-    CommonModule,
-    TransferHttpCacheModule,
-    HttpClientModule,
-    NgtUniversalModule,
+    AppModule,
+    BrowserTransferStateModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppBrowserModule {}
